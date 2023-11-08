bacc575e7e618540faff706cfdd298ab93da5711
chaitanya-ranjan29 <ranjanchaitanya29@gmail.com>
2023-11-08 21:05:34 +0530

    navbar 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,36 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Component/Home/Home";
 import Product from "./Component/Product/Product";
 import Navbar from "./Component/Navbar/Navbar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="product" element={<Product />} />
-
-      </Routes>
-    </BrowserRouter>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="product" element={<Product />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </React.Fragment>
   );
 }
 
 export default App;
+
+// export default function SimpleContainer() {
+//   return (
+//     <React.Fragment>
+//       <CssBaseline />
+//       <Container maxWidth="sm">
+//         <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+//       </Container>
+//     </React.Fragment>
+//   );
+// }
