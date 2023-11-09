2205e03569151f27576947f2c3bd30f45bfd0a8e
chaitanya-ranjan29 <ranjanchaitanya29@gmail.com>
2023-11-09 23:22:09 +0530

    navbarMenu

--- src/ui/NavbarMenu.jsx
+++ src/ui/NavbarMenu.jsx
@@ -1,15 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import MenuIcon from "@mui/icons-material/Menu";
-import CloseIcon from '@mui/icons-material/Close';
-import { Menu, MenuItem } from '@mui/material';
-import { categories } from '../../public/categories';
-import { Link } from '@mui/material';
+import CloseIcon from "@mui/icons-material/Close";
+import { Menu, MenuItem } from "@mui/material";
+import { categories } from "../../public/categories";
+import { Link } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
-
-
 
 function NavbarMenu() {
   const [menuClick, setMenuClick] = useState(false);
@@ -30,11 +28,11 @@ function NavbarMenu() {
   console.log(categories);
 
   return (
-    
-    <Toolbar style={{
-        padding: "auto 0px"
-      }}>
-
+    <Toolbar
+      style={{
+        padding: "auto 0px",
+      }}
+    >
       <IconButton
         size="large"
         edge="start"
@@ -42,25 +40,28 @@ function NavbarMenu() {
         aria-label="open drawer"
         onClick={handleMenuOpen}
       >
-        {menuClick ? (<CloseIcon />) : (<MenuIcon />)}
-        
+        {menuClick ? <CloseIcon /> : <MenuIcon />}
+
         <Typography>menu</Typography>
       </IconButton>
+
       <Menu anchorEl={anchorEl} open={open} onClose={handleMenuClose} >
-      <Typography>Shop by Categories</Typography>
-      {categories.map((category) => (
-        <Link key={category.id} component={RouterLink} to={`/${category.path}`} style={{textDecoration: "none", color: "inherit"}} >
-          <MenuItem onClick={handleMenuClose} >{category.name}</MenuItem>
-        </Link>
-      ))}
-        
-        {/* <MenuItem onClick={handleMenuClose} >Electronics</MenuItem> */}
+
+        <Typography>Shop by Categories</Typography>
+        {categories.map((category) => (
+          <Link
+            key={category.id}
+            component={RouterLink}
+            to={`/${category.path}`}
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <MenuItem onClick={handleMenuClose}>{category.name}</MenuItem>
+          </Link>
+        ))}
 
       </Menu>
-
     </Toolbar>
-
-  )
+  );
 }
 
 export default NavbarMenu;
