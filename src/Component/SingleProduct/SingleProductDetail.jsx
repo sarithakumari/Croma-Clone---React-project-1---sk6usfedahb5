61a1dea52730c4e5b994dde175400d60f6dc9cc4
chaitanya-ranjan29 <ranjanchaitanya29@gmail.com>
2023-12-05 19:14:34 +0530

    details

--- src/Component/SingleProduct/SingleProductDetail.jsx
+++ src/Component/SingleProduct/SingleProductDetail.jsx
@@ -5,13 +5,30 @@ import { useParams } from "react-router-dom";
 import ImageGallery from "react-image-gallery";
 // import stylesheet if you're not already using CSS @import
 import "react-image-gallery/styles/css/image-gallery.css";
-import { Box, CardActionArea, Container, Grid } from "@mui/material";
+import {
+  Box,
+  CardActionArea,
+  Container,
+  Grid,
+  Rating,
+  Typography,
+} from "@mui/material";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import SingleProductGallery from "./SingleProductGallery";
+import styled from "@emotion/styled";
+import { singleProductRatingReview } from "../../helper/singleProductRatingReview";
+
+const StyledRating = styled(Rating)({
+  "& .MuiRating-iconFilled": {
+    color: "#00e9bf",
+    fontSize: "14px",
+  },
+});
 
 function SingleProductDetail() {
   const [productDetails, setProductDetails] = useState(null);
   const [productImages, setProductImages] = useState([]);
+  const [productRatingReviews, setProductRatingReview] = useState(null);
   const { productID } = useParams();
 
   useEffect(() => {
@@ -19,24 +36,27 @@ function SingleProductDetail() {
       setProductDetails(data);
       setProductImages(data.images);
     });
+    singleProductRatingReview(productID).then((data) =>
+      setProductRatingReview(data)
+    );
   }, []);
 
   const images = productImages.map((image) => {
     return {
       original: image,
       thumbnail: image,
-      originalHeight: "100%"
+      originalHeight: "100%",
     };
   });
 
-  // console.log('details from singleproductdetails', product)
+  console.log("ratings from singleproductdetails", productRatingReviews);
+  // console.log("details from singleproductdetails", productDetails);
   // console.log('images from singleproductdetails', productImages)
-  console.log("images: ", images);
+  // console.log("images: ", images);
 
   return (
     <Container maxWidth="lg">
       <Grid container>
-
         <Grid item md={6} sm={12}>
           <SingleProductGallery images={images} />
         </Grid>
@@ -44,9 +64,70 @@ function SingleProductDetail() {
         <Grid item md={6} sm={12}>
           <div
             id="productDetails"
-            style={{ marginTop: "5rem", border: "1px solid white" }}
+            style={{
+              marginTop: "5rem",
+              // border: "1px solid white",
+              padding: "10px",
+            }}
           >
+            <Typography variant="h5" component="div">
+              {productDetails?.name}
+            </Typography>
 
+            <Box
+              component="div"
+              sx={{
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                gap: 0.2,
+                margin: "10px 0",
+                color: "#00e9bf",
+              }}
+            >
+              {productRatingReviews ? (
+                <>
+                  <Typography component="div" fontSize="14px">
+                    {productDetails?.ratings.toFixed(1)}
+                  </Typography>
+
+                  <StyledRating
+                    readOnly
+                    max={1}
+                    defaultValue={1}
+                    size="small"
+                  />
+
+                  <Typography
+                    variant="body2"
+                    component="div"
+                    sx={{ textDecoration: "underline", marginLeft: "5px" }}
+                  >
+                    ({productRatingReviews?.length} Ratings &{" "}
+                    {productRatingReviews?.length} Reviews)
+                  </Typography>
+
+                </>
+              ) : (
+                <Typography
+                  variant="body2"
+                  component="div"
+                  sx={{ textDecoration: "underline", marginLeft: "5px" }}
+                >
+                  (Be the First One to Review)
+                </Typography>
+              )}
+
+            </Box>
+
+            <Box component='div' id="productPrice">
+              <Box component='div' >
+                <Typography variant="h5" component="span" sx={{fontWeight: "500"}} > ₹ </Typography>
+                <Typography variant="h5" component="span" sx={{fontWeight: "500"}} > {productDetails?.price} </Typography>
+              </Box>
+              <Typography variant="body2" component="div" sx={{fontSize: "12px"}} > (Incl. all taxes.) </Typography>
+
+            </Box>
           </div>
         </Grid>
       </Grid>
