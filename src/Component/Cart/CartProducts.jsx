a7a01b8bd7cbb3fccc63b5900c6f208ab0ebda70
chaitanya-ranjan29 <ranjanchaitanya29@gmail.com>
2023-12-17 23:25:22 +0530

    responsive: cartProducts fixed

--- src/Component/Cart/CartProducts.jsx
+++ src/Component/Cart/CartProducts.jsx
@@ -61,56 +61,85 @@ function CartProducts({ product, handleSetCartProducts, handleClearCart }) {
     <Box
       component="div"
       sx={{
-        padding: "1rem 0 1rem 9rem",
+        padding: {
+          lg: "1rem 1rem 1rem 14rem",
+          md: "1rem 1rem 1rem 14rem",
+          sm: "12rem 1rem 1rem 1rem",
+          xs: "12rem 1rem 1rem 1rem",
+        },
         position: "relative",
-        marginRight: 1,
         borderRadius: "0.8rem",
         backgroundColor: "#ffffff",
-        marginBottom: 1,
-        width: "98%"
+        marginBottom: "1rem",
+        marginRight: {lg: "1rem", md: "1rem", sm: "0rem", xs: "0rem"}
       }}
     >
       {/* left box || right box */}
-      <Box component="div" sx={{ display: "flex", marginLeft: "1.5rem" }}>
-        <Box
-          component="div"
-          id="cartDetails-left"
-          sx={{ paddingRight: "1.5rem" }}
-        >
+      <Box
+        component="div"
+        sx={{
+          display: "flex",
+          width: "100%",
+          flexWrap: "wrap",
+        }}
+      >
+        <Box component="div" id="cartDetails-left" sx={{ width: "100%" }}>
+          {/* typo */}
           <CardActionArea
             disableRipple
             onClick={() => navigate(`/product/${product.product._id}`)}
           >
             <Typography
               component="p"
-              variant="h6"
               sx={{
                 overflow: "hidden",
                 textOverflow: "ellipsis",
                 display: "-webkit-box",
                 WebkitBoxOrient: "vertical",
                 WebkitLineClamp: "2",
-                fontWeight: "500",
+                fontWeight: "700",
+                fontSize: { lg: "18px", md: "18px", sm: "14px", xs: "12px" },
+                width: "100%",
               }}
             >
               {product.product.name}
             </Typography>
           </CardActionArea>
 
-          <Box component="div" marginTop={1}>
+          {/* rating */}
+          <Box
+            component="div"
+            marginTop={1}
+            sx={{
+              width: "100%",
+              "& .MuiRating-root": {
+                fontSize: { lg: "14px", md: "14px", sm: "12px", xs: "12px" },
+              },
+            }}
+          >
             <StyledRating
               value={product?.product?.ratings}
               readOnly
               precision={0.1}
-              size="small"
+              // size="small"
             />
           </Box>
 
-          <Box component="div" sx={{ display: "flex", marginTop: 1 }}>
+          {/* delivery texts */}
+          <Box
+            component="div"
+            sx={{ display: "flex", marginTop: 1, width: "100%" }}
+          >
             <Typography
               component="p"
               variant="body2"
-              sx={{ width: "150px", padding: "0 1rem 1rem 0", borderRadius: 1 }}
+              sx={{
+                width: "150px",
+                padding: "0 1rem 1rem 0",
+                borderRadius: 1,
+                marginRight: 2,
+                fontSize: { lg: "14px", md: "14px", sm: "10px", xs: "10px" },
+              }}
             >
               Standard delivery by tomorrow
             </Typography>
@@ -122,19 +151,26 @@ function CartProducts({ product, handleSetCartProducts, handleClearCart }) {
             <Typography
               component="p"
               variant="body2"
-              sx={{ width: "150px", padding: "0 0 1rem 1rem", borderRadius: 1 }}
+              sx={{
+                width: "150px",
+                padding: "0 0 1rem 1rem",
+                borderRadius: 1,
+                fontSize: { lg: "14px", md: "14px", sm: "10px", xs: "10px" },
+              }}
             >
               Express delivery by today
             </Typography>
           </Box>
 
-          <Box
+          {/* buttons */}
+          {/* <Box
             component="div"
             marginTop={1}
             sx={{
               "& .MuiButtonBase-root:hover ": {
                 border: "1px solid #12daa8",
               },
+              width: "100%",
             }}
           >
             <Button
@@ -145,10 +181,11 @@ function CartProducts({ product, handleSetCartProducts, handleClearCart }) {
                 borderRadius: "8px",
                 border: "1px solid black",
                 textTransform: "capitalize",
-                marginRight: 1.2,
+                // marginRight: 1.2,
                 fontSize: "12px",
                 padding: "10px 40px",
                 fontWeight: "700",
+                width: "180px",
               }}
               onClick={handleMoveProductToWishlist}
             >
@@ -161,30 +198,39 @@ function CartProducts({ product, handleSetCartProducts, handleClearCart }) {
                 borderRadius: "8px",
                 border: "1px solid black",
                 textTransform: "capitalize",
-                marginLeft: 1.2,
+                // marginLeft: 1.2,
                 fontSize: "12px",
                 padding: "10px 40px",
                 fontWeight: "700",
+                width: "180px",
               }}
               onClick={handleRemoveProductFromCart}
             >
               Remove
             </Button>
-          </Box>
+          </Box> */}
         </Box>
 
         <Box
           component="div"
           id="cartDetails-right"
-          sx={{ paddingRight: "1.5rem", width: "200px", textAlign: "end" }}
+          sx={{ width: "100%", textAlign: "end" }}
         >
-          <Box>
-            <Typography component="span" sx={{ fontSize: "24px" }}>
+          <Box component="div" sx={{}}>
+            <Typography
+              component="span"
+              sx={{
+                fontSize: { lg: "18px", md: "18px", sm: "14px", xs: "12px" },
+              }}
+            >
               ₹
             </Typography>
             <Typography
               component="span"
-              sx={{ fontSize: "24px", fontWeight: "700" }}
+              sx={{
+                fontSize: { lg: "18px", md: "18px", sm: "14px", xs: "12px" },
+                fontWeight: "700",
+              }}
             >
               {product.product.price.toLocaleString(navigator.language, {
                 minimumFractionDigits: 2,
@@ -196,7 +242,10 @@ function CartProducts({ product, handleSetCartProducts, handleClearCart }) {
             component="p"
             variant="body2"
             textAlign="end"
-            sx={{ padding: "1rem 0" }}
+            sx={{
+              padding: "1rem 0",
+              fontSize: { lg: "12px", md: "12px", sm: "10px", xs: "10px" },
+            }}
           >
             (Incl. all Taxes)
           </Typography>
@@ -208,7 +257,10 @@ function CartProducts({ product, handleSetCartProducts, handleClearCart }) {
               component="span"
               fontWeight="500"
               fontSize="14px"
-              sx={{ textDecoration: "line-through" }}
+              sx={{
+                textDecoration: "line-through",
+                fontSize: { lg: "16px", md: "16px", sm: "14px", xs: "12px" },
+              }}
             >
               MRP{" "}
             </Typography>
@@ -216,7 +268,10 @@ function CartProducts({ product, handleSetCartProducts, handleClearCart }) {
               component="span"
               fontWeight="500"
               fontSize="14px"
-              sx={{ textDecoration: "line-through" }}
+              sx={{
+                textDecoration: "line-through",
+                fontSize: { lg: "16px", md: "16px", sm: "14px", xs: "12px" },
+              }}
             >
               ₹
               {(product.product.price * 1.15).toLocaleString(
@@ -227,9 +282,11 @@ function CartProducts({ product, handleSetCartProducts, handleClearCart }) {
             <Typography
               component="p"
               fontWeight="500"
-              fontSize="12px"
               color="grey"
               textAlign="end"
+              sx={{
+                fontSize: { lg: "12px", md: "12px", sm: "10px", xs: "10px" },
+              }}
             >
               (Save ₹
               {(
@@ -243,12 +300,67 @@ function CartProducts({ product, handleSetCartProducts, handleClearCart }) {
           </Box>
           <Divider />
         </Box>
+
+        {/* buttons */}
+        <Box
+          component="div"
+          marginTop={1}
+          sx={{
+            "& .MuiButtonBase-root:hover ": {
+              border: "1px solid #12daa8",
+            },
+            width: "100%",
+            display: "flex",
+            justifyContent: {
+              lg: "space-around",
+              md: "space-around",
+              sm: "space-around",
+              xs: "space-between",
+            },
+          }}
+        >
+          <Button
+            disableRipple
+            sx={{
+              color: "#103749",
+              textAlign: "center",
+              borderRadius: "8px",
+              border: "1px solid black",
+              textTransform: "capitalize",
+              // marginRight: 1.2,
+              fontSize: { lg: "12px", md: "12px", sm: "10px", xs: "10px" },
+              padding: "10px 40px",
+              fontWeight: "700",
+              // width: {lg: "180px", md: "180px", sm: "150px", xs: "180px"},
+            }}
+            onClick={handleMoveProductToWishlist}
+          >
+            Move to Wishlist
+          </Button>
+          <Button
+            sx={{
+              color: "#103749",
+              textAlign: "center",
+              borderRadius: "8px",
+              border: "1px solid black",
+              textTransform: "capitalize",
+              // marginLeft: 1.2,
+              fontSize: { lg: "12px", md: "12px", sm: "10px", xs: "10px" },
+              padding: "10px 40px",
+              fontWeight: "700",
+              // width: {lg: "180px", md: "180px", sm: "120px", xs: "120px"},
+            }}
+            onClick={handleRemoveProductFromCart}
+          >
+            Remove
+          </Button>
+        </Box>
       </Box>
 
       {/* image */}
       <Box
         component="div"
-        sx={{ position: "absolute", top: "2rem", left: "3.6%" }}
+        sx={{ position: "absolute", top: "2rem", left: "2rem" }}
       >
         <CardActionArea
           disableRipple
@@ -258,8 +370,8 @@ function CartProducts({ product, handleSetCartProducts, handleClearCart }) {
             component="img"
             src={product.product.displayImage}
             alt="image"
-            height="100"
-            width="100"
+            height="150"
+            width="150"
             loading="lazy"
             sx={{
               objectFit: "contain",
